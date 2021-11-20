6c63b65539694b03943d121468680aeb293c6498
Trinh Tuan Kiet <76782169+TuaKie@users.noreply.github.com>
2021-11-20 08:53:47 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle:'Yoooooo !',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao bạn cứ đi nhầm đường
+    Lạc vào tim tớ lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'hihi',
+    title: 'UwU',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Tớ yeu thằng khác :)) ',
+    btnNo: 'Bạn bị ảo à ? ',
+    question:'Trên thế giới hơn 7 tỉ người mà sao bạn lại yeu th khác <3',
+    btnReply: 'Gửi cho bạn',
+    reply: ':)) Block',
+    mess: 'Tớ biết mà. ',
+    messDesc: 'Đùa tí thoiiii mà :)) Khum Block nhó !',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'http://fb.com' 
+}
