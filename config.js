3ba1f377d312697d63e5dddaf46ce5ee2b5b5a1b
Trinh Tuan Kiet <76782169+TuaKie@users.noreply.github.com>
2021-11-20 11:30:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 const CONFIG = {
     introTitle:'Yoooooo !',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao bạn cứ đi nhầm đường
-    Lạc vào tim tớ lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `Trái đất vốn lạ thường,
+    Mà sao bạn cứ đi nhầm đường,
+    Lạc vào tim tớ lẻ loi,
+    Đằng sau chữ yêu đây là thương.`,
     btnIntro: 'hihi',
-    title: 'UwU',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'Phai Chang Ban Da Yeu Ngay Tu Cai Nhin Dau Tien',
+    desc: 'Nếu Bạn Thoát Khỏi Trang Này Tức Là Bạn Đã Có Người Yêu Rồi Nhó !',
     btnYes: 'Tớ yeu thằng khác :)) ',
     btnNo: 'Bạn bị ảo à ? ',
     question:'Trên thế giới hơn 7 tỉ người mà sao bạn lại yeu th khác <3',
